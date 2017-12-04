1b49d1f09b51ea191abb4e5764a915b09a2e5d5b
Florentino <mosquitoix@icloud.com>
2017-12-04 14:01:28 +0800

    my demo

---
+++ src/main/java/cn/gov/gxdlr/portialdemo/PortialDemoApplication.java
@@ -0,0 +1,12 @@
+package cn.gov.gxdlr.portialdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortialDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortialDemoApplication.class, args);
+	}
+}
